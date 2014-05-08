7fa1e183348065bad8c3cc926275276a70eae9f0
adolfo <fitob612@gmail.com>
2014-05-08 18:02:41 +0200

    primeiros pasos con layouts en android + eclipse

---
+++ src/anuf/exemplo/welcome/DestinationActivity.java
@@ -0,0 +1,13 @@
+package anuf.exemplo.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DestinationActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_destination);
+	}
+
+}
---
+++ src/anuf/exemplo/welcome/NomeActivity.java
@@ -0,0 +1,13 @@
+package anuf.exemplo.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NomeActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_nome);
+	}
+
+}
